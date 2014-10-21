270333257f271d6eba0791e5030a118481d3cdbc
Anton Karpus <akarpus2@gmail.com>
2014-10-21 00:06:43 -0400

    Update Furnace.js

--- Furnace.js
+++ Furnace.js
@@ -1,40 +1,25 @@
 var http = require('http'); //need to http
 var url = require('url');
 
-var furnaceON = {
+var query = {
   hostname: 'localhost',
   port: '3000',
-  path: '/furnace?start=on'
+  path: '/furnace'
 }
 
-var furnaceOFF = {
-  hostname: 'localhost',
-  port: '3000',
-  path: '/furnace=off'
-}
-
-
-/*
-var temperature = 20;  //degrees celsius
-
-setInterval(function(){
-   console.log('temperature: ' + temperature++)}, 1000);
-*/
-
+setTimeout( function again(){
 function handleResponse(response){
   var serverData = '';
   response.on('data', function(chunk){serverData += chunk});
   
   response.on('end', function(){
-     console.log('Response Status: ', response.statusCode);
-     console.log('Response Headers: ',response.headers);
-     console.log(serverData);
-     });
+     if (serverData != '') console.log(serverData);
+  });
 }
 
-http.request(furnaceON, function(response){
-   handleResponse(response);
-}).end();
-
+http.request(query, function(response){
+   handleResponse(response);}).end();
+   setTimeout(again, 1000); //recursively restart timeout
+}, 1000);
 
 console.log('Client Running at http://127.0.0.1:3000  CNTL-C to quit');
